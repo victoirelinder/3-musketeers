5deb750b8e9d6d896b2a8ede41597002fb080420
victoirelinder <victoirelinder@hotmail.com>
2018-03-07 18:28:40 +0100

    add comments

--- cash/bin/index.js
+++ cash/bin/index.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
+// list of modules
 const Conf = require('conf');
 const helpers = require('./helpers.js');
 const cash = require('./cash.js');
 
 const config = new Conf();
 
-const argv = process.argv.slice(2);
+// get all the given parameters
 
+const argv = process.argv.slice(2);
+// it calls "helpers" which is a module , with the parameter argv 
 helpers(argv);
 
 const command = {
@@ -18,5 +21,5 @@ const command = {
       ? process.argv.slice(4)
       : config.get('defaultTo', ['USD', 'EUR', 'GBP', 'PLN'])
 };
-
+// it calls "cash " which is a module , with the parameter argv 
 cash(command);
